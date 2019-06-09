caae8e23a6cfab4c7b8303709e96e9d0494f4825
Oleg <olegtupicyn62@gmail.com>
2019-06-09 18:00:26 +0300

    HW admin

---
+++ home_work/07.06/Part 3/admin.js
@@ -0,0 +1,18 @@
+var user = prompt('Кто пришёл?', '');
+
+if (user == 'Админ') {
+    var pass = prompt('Пароль?', '');
+
+    if (pass == 'Черный Властелин') {
+        alert( 'Добро пожаловать!' );
+    } else if (pass == null) {
+        alert( 'Вход отменён' );
+    } else {
+        alert( 'Пароль неверен' );
+    }
+
+} else if (user == null) {
+    alert( 'Вход отменён' );
+} else {
+    alert( 'Я вас не знаю' );
+}
